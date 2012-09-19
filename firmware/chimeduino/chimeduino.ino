e728f9325b4459d2caff8b12ccd2335abd5c2b29
Kaj Schmidt <kaj.schmidt@it-gymnasiet.se>
2012-09-19 12:21:37 +0200

    Fully functional
    Can play song from array.

--- firmware/chimeduino/chimeduino.ino
+++ firmware/chimeduino/chimeduino.ino
@@ -10,23 +10,34 @@ License: CC-BY-SA 3.0
 
 */
 
-int BUTTON_PIN = 9;
+int BUTTON_PIN = 13;
 int BUTTON_TRIGGER;
 
 
-int SOLENOID_1 = 1;
-int SOLENOID_2 = 2;
-int SOLENOID_3 = 3;
-int SOLENOID_4 = 4;
+int SOLENOID_1 = 9;
+int SOLENOID_2 = 8;
+int SOLENOID_3 = 7;
+int SOLENOID_4 = 6;
 int SOLENOID_5 = 5;
-int SOLENOID_6 = 6;
-int SOLENOID_7 = 7;
-int SOLENOID_8 = 8;
+int SOLENOID_6 = 4;
+int SOLENOID_7 = 3;
+int SOLENOID_8 = 2;
 
 int NOTE_RETURN = 10;
-int NOTE_DELAY = 10;
+int NOTE_DELAY = 200;
 
 void setup() {
+    pinMode(SOLENOID_1,OUTPUT);
+    pinMode(SOLENOID_2,OUTPUT);
+    pinMode(SOLENOID_3,OUTPUT);
+    pinMode(SOLENOID_4,OUTPUT);
+    pinMode(SOLENOID_5,OUTPUT);
+    pinMode(SOLENOID_6,OUTPUT);
+    pinMode(SOLENOID_7,OUTPUT);
+    pinMode(SOLENOID_8,OUTPUT);
+
+    pinMode(BUTTON_PIN,INPUT);
+  
     digitalWrite(BUTTON_PIN, LOW);
 }
 
@@ -42,33 +53,76 @@ void loop() {
 void playdsong() {  // Play default song
   
     byte SONG[] = {
-    B00000001,
-    B00000010,
-    B00000100,
-    B00001000,
-    B00010000,
-    B00100000,
-    B01000000,
-    B10000000};
+      B00100000,
+      B00000000,
+      B00000000,
+      B00000000,
+      B00100000,
+      B00000000,
+      B00000000,
+      B00000000,
+      B00100000,
+      B00000000,
+      B00000000,
+      B00000000,
+      B11000000,
+      B00000000,
+      B00011000,
+      B00000000,
+      B00100000,
+      B00000000,
+      B00000000,
+      B00000000,
+      B11000000,
+      B00000000,
+      B00011000,
+      B00000000,
+      B00100000,
+      B00000000,
+      B00000000,
+      B00000000,
+      B00000000,
+      B00000100,
+      B00000000,
+      B00000000,
+      B00000000,
+      B00000100,
+      B00000000,
+      B00000000,
+      B00000000,
+      B00000100,
+      B00000000,
+      B00000000,
+      B00000001,
+      B00000000,
+      B00000100,
+      B00000000,
+      B00100000,
+      B00000000,
+      B00000000,
+      B11000000,
+      B00000000,
+      B00011000,
+      B00000000,
+      B00100000,
+    };    
+    int SONG_END = 53;
     
-    int SONG_END = 8;
-    int SONG_PLACE = 0;
-    
-    for (SONG_PLACE = 0; SONG_PLACE > SONG_END, SONG_PLACE++) {
+    for (int SONG_PLACE = 0; SONG_PLACE < SONG_END; SONG_PLACE++) {
        playnote(SONG[SONG_PLACE]);
        delay(NOTE_DELAY);
    }
 }
 
 void playnote(byte NOTE) {
-       if (BitRead(NOTE, 0) == 1) digitalWrite(SOLENOID_1,HIGH);
-       if (BitRead(NOTE, 1) == 1) digitalWrite(SOLENOID_2,HIGH);
-       if (BitRead(NOTE, 2) == 1) digitalWrite(SOLENOID_3,HIGH);
-       if (BitRead(NOTE, 3) == 1) digitalWrite(SOLENOID_4,HIGH);
-       if (BitRead(NOTE, 4) == 1) digitalWrite(SOLENOID_5,HIGH);
-       if (BitRead(NOTE, 5) == 1) digitalWrite(SOLENOID_6,HIGH);
-       if (BitRead(NOTE, 6) == 1) digitalWrite(SOLENOID_7,HIGH);
-       if (BitRead(NOTE, 7) == 1) digitalWrite(SOLENOID_8,HIGH);
+       if (bitRead(NOTE, 0) == 1) digitalWrite(SOLENOID_1,HIGH);
+       if (bitRead(NOTE, 1) == 1) digitalWrite(SOLENOID_2,HIGH);
+       if (bitRead(NOTE, 2) == 1) digitalWrite(SOLENOID_3,HIGH);
+       if (bitRead(NOTE, 3) == 1) digitalWrite(SOLENOID_4,HIGH);
+       if (bitRead(NOTE, 4) == 1) digitalWrite(SOLENOID_5,HIGH);
+       if (bitRead(NOTE, 5) == 1) digitalWrite(SOLENOID_6,HIGH);
+       if (bitRead(NOTE, 6) == 1) digitalWrite(SOLENOID_7,HIGH);
+       if (bitRead(NOTE, 7) == 1) digitalWrite(SOLENOID_8,HIGH);
        delay(NOTE_RETURN);
        digitalWrite(SOLENOID_1,LOW); //Return soleniods to endstate
        digitalWrite(SOLENOID_2,LOW);
